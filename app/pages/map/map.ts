81db05badf2a1ffb16e8bb9460ae3200250986ec
James Byrne <jamesdesbyrne@gmail.com>
2016-04-16 11:16:27 +0100

    Beautifiying map.ts

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -2,7 +2,7 @@ import {Page, Platform} from "ionic-angular";
 import {Http} from "angular2/http";
 import "rxjs/add/operator/map";
 
-import {Geolocation} from 'ionic-native';
+import {Geolocation} from "ionic-native";
 
 // Suppress typescript errors
 declare var L: any;
@@ -23,7 +23,7 @@ export class MapPage {
 
   private map: any;
   // Tell the app to use more accurate means of measurement if possible such as GPS
-  private options = {enableHighAccuracy: true};
+  private options = { enableHighAccuracy: true };
   private userMarker: any;
 
   constructor(platform: Platform, http: Http) {
@@ -37,59 +37,59 @@ export class MapPage {
   // Load the map
   loadMap() {
     this.platform.ready().then(() => {
-        
-        Geolocation.getCurrentPosition().then(pos => { 
-            // Create a new instance of the Leaflet map
-          this.map = L.map("map", {zoomControl: false}).setView([pos.coords.latitude, pos.coords.longitude], 13);
 
-          // Control for zooming in and out
-          let control = L.control.zoom({
-            position: "bottomright"
-          });
+      Geolocation.getCurrentPosition().then(pos => {
+        // Create a new instance of the Leaflet map
+        this.map = L.map("map", { zoomControl: false }).setView([pos.coords.latitude, pos.coords.longitude], 13);
 
-          // Add the control to the map
-          this.map.addControl(control);
-
-          // Add a tile layer to the map
-          L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-            maxZoom: 18
-          }).addTo(this.map);
-
-          // Create a custom icon for the user
-          let userIcon = L.icon({
-            iconUrl: "../../images/navigation-icon.png",
-            iconSize:     [40, 40], // size of the icon
-            iconAnchor:   [20, 20], // point of the icon which will correspond to marker"s location
-            popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-          });
-          
-          // Add a user marker which shows their current location
-          // The zIndexOffset ensure that the users marker is always ontop of any other markers nearby
-          this.userMarker = L.marker([pos.coords.latitude, pos.coords.longitude], {
-            zIndexOffset: 1000,
-            icon:  userIcon,
-          }).addTo(this.map);
-
-          // Add a popup to the userMarker
-          this.userMarker.bindPopup("<h4>Your Location</h4>");
-
-          // Follow the Users position
-          this.followUser();
-
-          // Get the list of food places in dublin
-          // this.getDublinFood();
-        }).catch(err => {
-          console.log("There was an error getting the map ");
-          console.log(err);            
+        // Control for zooming in and out
+        let control = L.control.zoom({
+          position: "bottomright"
         });
+
+        // Add the control to the map
+        this.map.addControl(control);
+
+        // Add a tile layer to the map
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          maxZoom: 18
+        }).addTo(this.map);
+
+        // Create a custom icon for the user
+        let userIcon = L.icon({
+          iconUrl: "../../images/navigation-icon.png",
+          iconSize: [40, 40], // size of the icon
+          iconAnchor: [20, 20], // point of the icon which will correspond to marker"s location
+          popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+        });
+
+        // Add a user marker which shows their current location
+        // The zIndexOffset ensure that the users marker is always ontop of any other markers nearby
+        this.userMarker = L.marker([pos.coords.latitude, pos.coords.longitude], {
+          zIndexOffset: 1000,
+          icon: userIcon,
+        }).addTo(this.map);
+
+        // Add a popup to the userMarker
+        this.userMarker.bindPopup("<h4>Your Location</h4>");
+
+        // Follow the Users position
+        this.followUser();
+
+        // Get the list of food places in dublin
+        // this.getDublinFood();
+      }).catch(err => {
+        console.log("There was an error getting the map ");
+        console.log(err);
+      });
     });
   }
 
   // Follow user
   followUser() {
-    // Follow the users location 
+    // Follow the users location
     let watch = Geolocation.watchPosition();
-    // When the users location changes update the location of the marker 
+    // When the users location changes update the location of the marker
     watch.subscribe(pos => {
       this.userMarker.setLatLng([pos.coords.latitude, pos.coords.longitude]);
     });
